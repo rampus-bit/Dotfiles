9904e69d6cddc6cbe1cef2ae48294f3a3bd4a042
Alden Garcia <76920905+rampus-bit@users.noreply.github.com>
2021-08-06 23:00:37 -0400

    Update autostart.sh

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -3,15 +3,15 @@
 WM=$DESKTOP_SESSION
 
 function run {
-	if ! pgrep $1 ;
-	then
-			$@&
-	fi
+    if ! pgrep $1 ;
+    then
+    	$@&
+    fi
 }
 
 if [[ $WM = i3 ]]
 then
-	sudo mount -b /dev/sdb1
+    sudo mount -b /dev/sdb1
 fi
 
 picom --config $HOME/.config/bspwm/picom.conf &
